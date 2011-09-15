670587aab7e09a1c0b35b466a92e48be0f7c938a
Christopher Colvard <cjcolvar@pawpaw.dlib.indiana.edu>
2011-09-15 09:49:57 -0400

    Initial Commit: empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hydrant::Application.config.secret_token = '34b9615f8d55ba95b3d4df586baee7741cdee7d807b226d42b0d6c988edfd7a10a048a478d865a099789204c258f7aa5a9be1642de55685813211ab449f6563e'
