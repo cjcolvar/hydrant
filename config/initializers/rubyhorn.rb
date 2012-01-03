fb051b5377aa1846611be0cf9891112653d869a3
Chris Colvard <cjcolvar@indiana.edu>
2012-01-03 11:52:20 -0500

    Moved fedora config files to match new rake tasks.  Added rubyhorn initializer.

---
+++ config/initializers/rubyhorn.rb
@@ -0,0 +1,6 @@
+require 'rubyhorn'
+
+config = {:uri=>"http://pawpaw.dlib.indiana.edu:8080/",
+          :user=>'matterhorn_system_account',
+          :password=>'CHANGE_ME' }
+Rubyhorn.connect(config)
